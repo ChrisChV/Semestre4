a5755da310075c1894d4e6be19fdbf0f663b25e3
Chris <cristo2014f@outlook.es>
2015-09-04 12:29:28 -0500

    base de datos

--- UUU/main.cpp
+++ UUU/main.cpp
@@ -49,7 +49,7 @@ protected:
 // ----------------------------------------------------------------------------
 #if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
 #define WIN32_LEAN_AND_MEAN
-#include "windows.h"
+#include " .h"
 INT WINAPI WinMain( HINSTANCE hInst, HINSTANCE, LPSTR strCmdLine, INT )
 #else
 int main(int argc, char **argv)
