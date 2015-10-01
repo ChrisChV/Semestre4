131cb9911a8a669f5b64d648a2606912ded614f6
CHris <cristo2014f@outlook.es>
2015-10-01 12:10:23 -0500

    graphiz

--- AED CHRISTIAN/Monticulo/include/Monticulo.h
+++ AED CHRISTIAN/Monticulo/include/Monticulo.h
@@ -2,15 +2,19 @@
 #define MONTICULO_H
 #include "vector"
 #include "algorithm"
+#include "fstream"
 
 using namespace std;
 
 int _izquierda(int);
 int _derecha(int);
+int _padre(int);
 template <typename T>
 void minheapFy(vector<T> &, int);
 template <typename T>
 void maxheapFy(vector <T> &, int);
+template <typename T>
+void maxheapFy(vector <T> &, int,int);
 template <typename T>
 bool esHoja(vector<T> &, int);
 template <typename T>
@@ -19,17 +23,46 @@ template <typename T>
 void makeMaxHeap(vector<T> &);
 template <typename T>
 void heapSort(vector<T> &);
+template <typename T>
+void print(vector<T> &);
+template <typename T>
+void minHeapDecreaseKey(vector<T> &, int);
+
+
+template<typename T>
+void minHeapDecreaseKey(vector<T> &vec, int index){
+    if(index == 0)return;
+    if(vec[index] > vec[_padre(index)])swap(vec[index],vec[_padre(index)]);
+    minHeapDecreaseKey(vec,_padre(index));
+}
+
+template <typename T>
+void print(vector<T> &vec){
+    ofstream monti("monti.dot");
+    if(monti.fail()){
+        cout<<"El archvio no se pudo abrir"<<endl;
+        return;
+    }
+    monti<<"digraph{"<<endl;
+    for(int i = 0; i < vec.size(); i++){
+        if(_izquierda(i) < vec.size())monti<<vec[i]<<"->"<<vec[_izquierda(i)]<<endl;
+        if(_derecha(i) < vec.size())monti<<vec[i]<<"->"<<vec[_derecha(i)]<<endl;
+    }
+    monti<<"}";
+    monti.close();
+}
 
 template <typename T>
 void heapSort(vector<T> &vec){
     makeMaxHeap(vec);
     auto b = vec.size();
     auto s = vec.size() - 1;
-    for(int i = 0; i < b; i++){
-        swap(vec[0], s);
+    for(int i = 0; i < b - 1; i++){
+        swap(vec[0], vec[s]);
         s--;
-        maxheapFy(vec,0);
+        maxheapFy(vec,0,s);
     }
+
 }
 
 template <typename T>
@@ -49,8 +82,8 @@ void makeMinHeap(vector<T> &vec){
 
 template <typename T>
 bool esHoja(vector <T> &vec, int index){
-    auto b = vec.size() - 1;
-    if(vec[_derecha(index)] > b and vec[_izquierda(index)] > b)return true;
+    auto b = vec.size();
+    if(_derecha(index) > b and _izquierda(index) > b)return true;
     return false;
 }
 
@@ -62,12 +95,15 @@ int _izquierda(int index){
     return 2 * index + 1;
 }
 
+int _padre(int index){
+    return (index - 1)/2
+}
 template <typename T>
 void minheapFy(vector<T> &vec, int index){
     if(esHoja(vec,index))return;
     int menor = index;
-    if(vec[_izquierda(index)] > vec[menor])menor = _izquierda(index);
-    if(vec[_derecha(index)] > vec[menor])menor = _derecha(index);
+    if(vec[_izquierda(index)] < vec[menor])menor = _izquierda(index);
+    if(vec[_derecha(index)] < vec[menor] and _derecha(index) < vec.size())menor = _derecha(index);
     if(menor == index)return;
     swap(vec[index], vec[menor]);
     minheapFy(vec, menor);
@@ -75,14 +111,26 @@ void minheapFy(vector<T> &vec, int index){
 }
 
 template <typename T>
+void maxheapFy(vector<T> &vec, int index, int tam){
+    if(_izquierda(index) > tam)return;
+    if(esHoja(vec,index))return;
+    int mayor = index;
+    if(vec[_izquierda(index)] > vec[mayor])mayor = _izquierda(index);
+    if(vec[_derecha(index)] > vec[mayor] and _derecha(index) < tam)mayor = _derecha(index);
+    if(mayor == index)return;
+    swap(vec[index], vec[mayor]);
+    maxheapFy(vec, mayor,tam);
+}
+
+template <typename T>
 void maxheapFy(vector<T> &vec, int index){
     if(esHoja(vec,index))return;
-    int menor = index;
-    if(vec[_izquierda(index)] < vec[menor])menor = _izquierda(index);
-    if(vec[_derecha(index)] < vec[menor])menor = _derecha(index);
-    if(menor == index)return;
-    swap(vec[index], vec[menor]);
-    maxheapFy(vec, menor);
+    int mayor = index;
+    if(vec[_izquierda(index)] > vec[mayor])mayor = _izquierda(index);
+    if(vec[_derecha(index)] > vec[mayor])mayor = _derecha(index);
+    if(mayor == index)return;
+    swap(vec[index], vec[mayor]);
+    maxheapFy(vec, mayor);
 }
 
 
--- AED CHRISTIAN/Monticulo/main.cpp
+++ AED CHRISTIAN/Monticulo/main.cpp
@@ -5,6 +5,14 @@ using namespace std;
 
 int main()
 {
+    vector<int> a;
+    for(int i = 10; i > 0; i--){
+        a.push_back(i);
+    }
+    heapSort(a);
+    for(int i = 0; i < a.size(); i++){
+        cout<<a[i]<<"->";
+    }
 
 
 }
